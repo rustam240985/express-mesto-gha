d41064089e4e69a8b19e8c0380c0bb56b33b685f
Rustam Nigamatullin <rust2409@ya.ru>
2023-04-14 01:49:09 +0500

    fix: исправить ошибки теста

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const usersRouter = require('./users');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.all('/*', (req, res) => {
+  res.status(404).send('Sorry cant find that!');
+});
 
 module.exports = router;
