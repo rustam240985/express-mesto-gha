305372a232717a58cadd843a7c960bd2d8b3418c
Rustam Nigamatullin <rust2409@ya.ru>
2023-04-14 02:15:11 +0500

    fix: исправить ошибки теста

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const usersRouter = require('./users');
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 router.all('/*', (req, res) => {
-  res.status(404).send('Sorry cant find that!');
+  res.status(404).json({ message: 'Такая страница не существует' });
 });
 
 module.exports = router;
